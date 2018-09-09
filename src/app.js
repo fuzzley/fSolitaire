00e8a3b340bbc1830bda820828c96bf64c90aab9
fuzzley <fuzz357@hotmail.com>
2018-09-09 16:27:00 -0700

    Setup initial project structure and add Phaser TS typings.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var App = (function () {
+    function App() {
+    }
+    App.run = function () {
+        console.log('Hello World!');
+    };
+    return App;
+}());
+App.run();
+//# sourceMappingURL=app.js.map

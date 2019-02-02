0d2b6c7390342c89ce063637f432112eac72c328
fuzzley <andrey.gavel@gmail.com>
2019-02-02 14:24:35 -0800

    Add prettier config and remove log statements.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import 'phaser';
 
 class App {
     public run() {
-        console.log('Hello');
         new Phaser.Game();
-        console.log('Bye');
     }
 }
 
-console.log('We at least got here..');
 const app = new App();
 app.run();
